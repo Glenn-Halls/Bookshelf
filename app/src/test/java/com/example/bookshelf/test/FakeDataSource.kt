f561735f37b71a72b7293efebef3de816b058233
Glenn Halls <gjhalls@gmail.com>
2023-07-26 14:31:57 +1000

    Add: fake data source

---
+++ app/src/test/java/com/example/bookshelf/test/FakeDataSource.kt
@@ -0,0 +1,24 @@
+package com.example.bookshelf.test
+
+import com.example.bookshelf.network.Book
+import com.example.bookshelf.network.SearchResult
+
+
+object FakeDataSource {
+    private const val BookName1 = "Book01"
+    private const val BookName2 = "Book02"
+    private const val BookName3 = "Book03"
+    private const val BookLink1 = "BookLink01"
+    private const val BookLink2 = "BookLink02"
+    private const val BookLink3 = "BookLink03"
+    private val bookList = listOf(
+        Book(BookName1, BookLink1),
+        Book(BookName2, BookLink2),
+        Book(BookName3, BookLink3),
+    )
+    // Mimics result from Network containing List of three books
+    val searchResult: SearchResult = SearchResult(
+        totalItems = bookList.size,
+        items = bookList
+    )
+}
